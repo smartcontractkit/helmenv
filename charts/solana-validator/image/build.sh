d67ee63be5330cd9f93f796ef9d35b0a16076ad1
Tate <tate.exon@smartcontract.com>
2022-03-31 07:54:53 -0600

    remove dockerfile since it is not needed

--- charts/solana-validator/image/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TAG=1.9.14
-
-docker build --build-arg TAG=${TAG}  -t tateexon/solana-validator:${TAG} .
-
-# example docker push, to be updated when we have a place in ecr
-# docker push tateexon/solana-validator:${TAG}
